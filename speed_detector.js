a24b07449fc4d046338a907ae8da32dbe2c3754b
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-11-28 20:42:03 +0300

    Done with speed detector challenge

--- speed_detector.js
+++ speed_detector.js
@@ -9,7 +9,8 @@ let speed = prompt("Speed in km: ")
 //function to calculate demerit points for every 5km above speed limit of 70km
 //demerit points should be a max of 12 points. Beyond that license is suspended
 function carSpeed () {
-    let points = ((speed - 70) / 5)
+    //Math.floor is used to round down so that it the demerit point only change after every 5km
+    let points = Math.floor(((speed - 70) / 5))
     if (speed <= 70){
         return "Ok"
     } else if (speed > 70 && points <= 12) {
